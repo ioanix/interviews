69a06191d9588be4b0cf52c463a904dfc9dda1f7
ioanix <ioana_1001@yahoo.com>
2020-10-07 17:17:41 +0300

    "rotate array"

---
+++ leetcode/arrays/RotateArray_leetcode/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[] {1, 2, 3, 4, 5};
+        int k = 2;
+
+        rotate(nums, k);
+
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void rotate(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+
+        while(start < end) {
+
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
